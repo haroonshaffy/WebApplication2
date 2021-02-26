25e27900ca91059c27ca48303f8c5194633b3fe1
haroonshaffy <78490441+haroonshaffy@users.noreply.github.com>
2021-02-27 05:09:29 +0530

    Add project files.

---
+++ WebApplication2/Controllers/OccupyParkingController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+    /// <summary>
+    /// Tells us how many parking slots were filled on both days
+    /// </summary>
+    /// <param name="parkingSpots">Number of parking spots</param>
+    /// <param name="yspots"> Parking spots occuppied and unoccupied on the previous day</param>
+    /// <param name="tspots"> Parking spots occuppied and unoccupied today</param>
+    /// <example> GET api/OccupyParking/Parking/5/CCUCU/CUUCU -> 2 parking spots were occupied both yesterday and today</example>
+    /// <example> GET api/OccupyParking/Parking/5/CUCUC/CCCCC -> 3 parking spots were occupied both yesterday and today</example>
+    /// <returns> The number of parking spots which were occupied on both the days</returns>
+    public class OccupyParkingController : ApiController
+    {
+        [HttpGet]
+        [Route("api/OccupyParking/Parking/{parkingSpots}/{yspots}/{tspots}")]
+        public string Parking(int parkingSpots, string yspots, string tspots)
+        {
+            string output = "";
+            int counter = 0;
+            
+
+            string YUpper = yspots.ToUpper();
+            string TUpper = tspots.ToUpper();
+
+            char[] Ychars = YUpper.ToCharArray();
+            char[] Tchars = TUpper.ToCharArray();
+            
+            for(int i=0; i<parkingSpots; i++)
+            {
+                if (Ychars[i] == Tchars[i] && Ychars[i] == 'C')
+                {
+                    counter = counter + 1;
+
+                }
+            }
+            output = counter + " parking spots were occupied both yesterday and today";
+            return output;
+        }
+    }
+}
---
+++ WebApplication2/Controllers/TelemarketerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+    /// <summary>
+    /// Checks if the number is from a telemarketer or not and tells the user to either answer 
+    /// or ignore the call
+    /// </summary>
+    /// <param name="num1"> The first of the last four digits</param>
+    /// <param name="num2"> The second of the last four digits</param>
+    /// <param name="num3"> The third of the last four digits</param>
+    /// <param name="num4"> The last digit</param>
+    /// <example> GET api/Telemarketer/FourDigits/9/6/6/8 -> Ignore</example>
+    /// <example> GET api/Telemarketer/FourDigits/5/6/6/8 -> Answer</example>
+    /// <returns> Either Ignore or Answer based on if the call is from a 
+    /// telemarketer or not</returns>
+    public class TelemarketerController : ApiController
+    {
+        [HttpGet]
+        [Route("api/Telemarketer/FourDigits/{num1}/{num2}/{num3}/{num4}")]
+        
+        public string FourDigits(int num1, int num2, int num3, int num4)
+        {
+            if((num1 == 8 || num1 == 9) && (num4 == 8 || num4 ==9) && (num2 == num3))
+            {
+                return "Ignore";
+            }
+            else
+            {
+                return "Answer";
+            }
+        }
+    }
+}
